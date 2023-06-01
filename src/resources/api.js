5c279fe0bb7df60040cb9e11563f149ac9fc59d0
Orlando <opseua@gmail.com>
2023-06-01 02:48:03 -0300

    Função 'api' atualizada

--- src/resources/api.js
+++ src/resources/api.js
@@ -1,56 +1,61 @@
-async function api(val) {
+async function api(inf_ok) {
 
   const inf = {
-    url: val.url,
-    method: val.method,
-    headers: val.headers,
-    body: val.body
+    url: inf_ok.url,
+    method: inf_ok.method,
+    headers: inf_ok.headers,
+    body: inf_ok.body
   };
 
-  if (typeof fetch == undefined) { // -------------------- Google App Script
+  const ret = { ret: false };
+
+  if (typeof fetch == "undefined") { // ################ Google App Script
     try {
       var req = UrlFetchApp.fetch(inf.url, {
         'method': inf.method,
-        'payload': inf.method === "POST" || inf.method === "PATCH" ? typeof inf.body === 'object' ? JSON.stringify(inf.body) : inf.body : null,
+        'payload': inf.method === "POST" ? typeof inf.body === 'object' ? JSON.stringify(inf.body) : inf.body : null,
         'headers': inf.headers,
         redirect: 'follow',
         keepalive: true,
         muteHttpExceptions: true,
         validateHttpsCertificates: true,
       });
-      //console.log('API: OK Google App Script');
-      return req.getContentText();
+      //console.log('API: OK');
+      ret['ret'] = true;
+      ret['res'] = req.getContentText();
     } catch (error) {
-      //console.log('API: ERRO Google App Script');
-      return error.message;
+      console.log('API: ERRO');
+      //return error.message;
+      ret['res'] = error.message;
     }
-  } else { // ---------------------------------------------- JavaScript
+  } else { // ######################################### JavaScript
     try {
       var req = await fetch(inf.url, {
         method: inf.method,
-        body: inf.method === "POST" || inf.method === "PATCH" ? typeof inf.body === 'object' ? JSON.stringify(inf.body) : inf.body : null,
+        body: inf.method === "POST" ? typeof inf.body === 'object' ? JSON.stringify(inf.body) : inf.body : null,
         headers: inf.headers,
         redirect: 'follow',
         keepalive: true
       });
-      //console.log('API: OK JavaScript');
-      return await req.text();
+      //console.log('API: OK');
+      ret['ret'] = true;
+      ret['res'] = await req.text();
     } catch (error) {
-      //console.log('API: ERRO JavaScript');
-      return error;
+      console.log('API: ERRO');
+      //return error;
+      ret['res'] = error;
     }
   }
+
+  return ret
 }
 export default api
 
 
 
-// ###################### EXEMPLOS DE COMO FAZER A REQUISICAO ######################
-
-/* 
 
 
-async function teste() {
+/* async function teste() {
   const corpo = String.raw`ESSA \ É / " A ' INFORMACAO`;
   //const corpo = { teste: 'OLA TUDO BEM' };
   const requisicao = {
@@ -59,18 +64,19 @@ async function teste() {
     headers: { 'Content-Type': 'application/json' },
     body: valor
   };
-  const re = await api(requisicao);
-  console.log(re)
+  const retApi = await api(requisicao);
+  console.log(retApi.res)
 }
-teste()
+teste() */
 
 
-async function teste() {
+
+/* async function teste() {
   // POST → x-www-form-urlencoded
   const formData = new URLSearchParams();
   formData.append('grant_type', 'client_credentials');
-  formData.append('client_id', 'c683fd93-7db0-44a5-98a3-7fe0890b5c90');
-  formData.append('client_secret', '0008Q~5O2MA2Q-YeMJQN9de_4pPVzx8tse096c38');
+  formData.append('client_id', 'c683fd93-XXXXXXXXXXXXX');
+  formData.append('client_secret', '0008Q~XXXXXXXXXX');
   formData.append('resource', 'https://graph.microsoft.com');
   const corpo = formData.toString()
   const requisicao = {
@@ -79,10 +85,7 @@ async function teste() {
     headers: { 'Content-Type': 'application/x-www-form-urlencoded' },
     body: corpo
   };
-  const re = await api(requisicao);
-  console.log(re)
+  const retApi = await api(requisicao);
+  console.log(retApi.res)
 }
-teste() 
-
-
-*/
+teste() */
